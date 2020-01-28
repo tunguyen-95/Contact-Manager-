a48d4b46aa7f985595d7e977254ad70a1670b952
tunguyen-95 <thunguyen.tt@outlook.com>
2020-01-28 12:35:48 +0100

    refactoring to async/await

--- config/db.js
+++ config/db.js
@@ -2,17 +2,17 @@ const moongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  moongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await moongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
-    })
-    .then(() => console.log('MongoDB Connected '))
-    .catch(err => {
-      console.error(err.message);
-      process.exit(1);
     });
+    console.log('MongoDB Connected ');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 module.exports = connectDB;
