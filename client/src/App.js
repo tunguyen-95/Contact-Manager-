4ba2c8d79a4193bb276b636fda54464437196879
tunguyen-95 <thunguyen.tt@outlook.com>
2020-02-07 13:32:24 +0100

    set up client app, Navbar and clean up

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      MY APP
+    </div>
+  );
+};
+
+export default App;
