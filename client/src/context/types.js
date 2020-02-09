26f5dd50abb9effa0db9c9ac74dc47a2bb930a0f
tunguyen-95 <thunguyen.tt@outlook.com>
2020-02-09 12:54:40 +0100

    fix unresolved components

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,5 +1,4 @@
 export const ADD_CONTACT = 'ADD_CONTACT';
-export const ADD_CONTACT = 'ADD_CONTACT';
 export const DELETE_CONTACT = 'DELETE_CONTACT';
 export const SET_CURRENT = 'SET_CURRENT';
 export const CLEAR_CURRENT = 'CLEAR_CURRENT';
