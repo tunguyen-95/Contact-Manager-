f6acd3216af7da097518323530ddb7f100a025e7
tunguyen-95 <thunguyen.tt@outlook.com>
2020-02-10 23:30:10 +0100

    add Login form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
