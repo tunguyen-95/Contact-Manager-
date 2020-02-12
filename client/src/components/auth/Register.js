ab4cda5f558a6388ece42e3906e2156e0ce81357
tunguyen-95 <thunguyen.tt@outlook.com>
2020-02-12 11:17:48 +0100

    User registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,17 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
   const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
-
+  const { register, error, clearErrors } = authContext;
   const [user, setUser] = useState({
     name: '',
     email: '',
     password: '',
     password2: ''
   });
+  useEffect(() => {
+    if (error === 'User already exists') {
+      setAlert(error, 'DANGER');
+      clearErrors();
+    }
+  }, [error]);
 
   const { name, email, password, password2 } = user;
 
@@ -24,7 +32,11 @@ const Register = () => {
     } else if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('Register submit');
+      register({
+        name,
+        email,
+        password
+      });
     }
   };
 
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -23,16 +23,43 @@ const AuthState = props => {
   };
 
   const [state, dispatch] = useReducer(authReducer, initialState);
-  
-  //Set alert
+
+  //Load User
+  const loadUser = () => console.log('Load User');
 
   //Register User
+  const register = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
 
+    try {
+      const res = await axios.post('api/users', formData, config);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
   //Login User
 
+  const login = () => console.log('Login  User');
+
   //Logout
+  const logout = () => console.log('Load User');
 
   //Clear Errors
+
+  const clearErrors = () => {
+    dispatch({ type: CLEAR_ERRORS });
+  };
 
   return (
     <AuthContext.Provider
@@ -41,7 +68,9 @@ const AuthState = props => {
         isAuthenticated: state.isAuthenticated,
         loading: state.loading,
         user: state.user,
-        error: state.error
+        error: state.error,
+        register,
+        clearErrors
       }}
     >
       {props.children}
