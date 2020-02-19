4639f9b610ae63ebadbd233fef31b619bfaf3efd
tunguyen-95 <thunguyen.tt@outlook.com>
2020-02-19 15:13:14 +0100

    delete contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -69,14 +69,26 @@ const ContactState = props => {
   };
 
   //Delete Contact
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
-  };
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
 
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
   //Clear Contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
   };
+
   //Set Current Contact
   const setCurrent = contact => {
     dispatch({ type: SET_CURRENT, payload: contact });
